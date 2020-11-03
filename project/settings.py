470ce2734d9e5030dfafc080b9c79c36d556c374
werdani <https://github.com/werdani/django-job-board.git>
2020-11-04 01:18:30 +0200

    add block and search

--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.conf import settings
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,5 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS =[
-   os.path.join(BASE_DIR,"static"),
+   os.path.join(BASE_DIR,'static'),
 ]
+MEDIA_URL ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
