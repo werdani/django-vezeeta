cabcfce04dfb85215f278445e98819fe44c2ad5d
werdani <https://github.com/werdani/django-job-board.git>
2020-11-04 12:34:55 +0200

    add django signals

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
+from django.db.models.signals import post_save
+
 
 # Create your models here.
 
@@ -8,7 +10,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User,verbose_name=_("user"), on_delete=models.CASCADE)
     name = models.CharField(_("الاسم :"), max_length=50)
     who_i = models.TextField(_("من انا :"))
-    price = models.IntegerField(_("سعر الكشف :")) 
+    price = models.IntegerField(_("سعر الكشف :"),blank=True,null=True) 
     image = models.ImageField(_("الصوره الشخصيه :"), upload_to='profile')
     
 
@@ -20,6 +22,12 @@ class Profile(models.Model):
         verbose_name_plural = _("Profiles")
 
     def __str__(self):
-        return self.name
+        return '%s' %(self.user.username)
 
+def create_profile(sender,**kwargs):
+    if kwargs['created']:
+        Profile.objects.create(user=kwargs['instance'])
+
+
+post_save.connect(create_profile,sender=User)
     
