6e1aba3f322b54d1227393fa21563c48b7345439
werdani <https://github.com/werdani/django-job-board.git>
2020-11-04 13:12:30 +0200

    add slug

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 from django.db.models.signals import post_save
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -11,7 +12,14 @@ class Profile(models.Model):
     name = models.CharField(_("الاسم :"), max_length=50)
     who_i = models.TextField(_("من انا :"))
     price = models.IntegerField(_("سعر الكشف :"),blank=True,null=True) 
-    image = models.ImageField(_("الصوره الشخصيه :"), upload_to='profile')
+    image = models.ImageField(_("الصوره الشخصيه :"), upload_to='profile',blank=True, null=True)
+    slug = models.SlugField(_("slug"),blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.user.username)
+       
+        super(Profile, self).save(*args, **kwargs) # Call the real save() method
     
 
     
