981698cd1ee0942badd2eccec6e404e797a91916
werdani <https://github.com/werdani/django-job-board.git>
2020-11-05 19:01:08 +0200

    edit model profile

--- accounts/models.py
+++ accounts/models.py
@@ -6,23 +6,50 @@ from django.utils.text import slugify
 
 
 # Create your models here.
+TYPE_OF_PERSON=(
+    ("M","Male"),
+    ("F","Female"),
+)
 
 class Profile(models.Model):
+    DOCTOR_IN={
+        ("جلديه","جلديه"),
+        ("اسنان","اسنان"),
+        ("نفسي","نفسي"), 
+        ("اطفال حديثي الولاده","اطفال حديثي الولاده"),
+        ("مخ واعصاب","مخ واعصاب"),
+        ("عظام","عظام"),
+        ("نساء وتوليد","نساء وتوليد"),
+        ("انف واذن وحنجره","انف واذن وحنجره"),
+        ("قلب واعويه دمويه","قلب واعويه دمويه"),
+        ("امراض دم","امراض دم"),
+        ("اورام","اورام"),
+        ("باطنه","باطنه"),
+        ("تخسيس وتغذيه","تخسيس وتغذيه"),
+        ("جراحه اطفال","جراحه اطفال"),
+        ("جراحه اورام","جراحه اورام"),
+        ("جراحه اوعيه دمويه","جراحه اوعيه دمويه"),
+        ("جراحه تجميل","جراحه تجميل"),
+        ("جراحه سمنه مناطير","جراحه سمنه مناطير"),
+               
+    }
     user = models.OneToOneField(User,verbose_name=_("user"), on_delete=models.CASCADE)
     name = models.CharField(_("الاسم :"), max_length=50)
+    surname = models.CharField(_("اللقب :"), max_length=50)
     subtitle = models.CharField(_("نبذه عنك :"), max_length=50)
     address = models.CharField(_("المحافظه :"), max_length=50)
     address_detail = models.CharField(_("العنوان بالتفصيل :"), max_length=150)
     phone_number = models.CharField(_("الهاتف :"), max_length=50)
     working_hours = models.CharField(_("عدد ساعات العمل :"), max_length=50)
     wating_time = models.CharField(_("مده الانتظار :"), max_length=50,blank=True, null=True)
-    doctor = models.CharField(_("دكتور ؟"), max_length=50,blank=True, null=True)
     who_i = models.TextField(_("من انا :"))
     price = models.IntegerField(_("سعر الكشف :"),blank=True,null=True) 
     facbook = models.CharField( max_length=50,blank=True,null=True)
     twitter = models.CharField(max_length=50,blank=True,null=True)
     google = models.CharField(max_length=50,blank=True,null=True)
-
+    join_new = models.DateTimeField(_("وقت الانضمام :"),auto_now_add=True,blank=True,null=True)
+    type_preson = models.CharField(_("النوع :"),choices = TYPE_OF_PERSON ,max_length=50)
+    doctor = models.CharField(_("دكتور ؟"),choices= DOCTOR_IN, max_length=50,blank=True, null=True)
     image = models.ImageField(_("الصوره الشخصيه :"), upload_to='profile',blank=True, null=True)
     specialist_doctor = models.CharField(_("متخصص في :"), max_length=50,blank=True, null=True)
     slug = models.SlugField(_("slug"),blank=True, null=True)
